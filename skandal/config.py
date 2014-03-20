6072369dfa65bda2e73c01947f8595774227701d
sergeLabo <sergeblender@gmail.com>
2014-03-20 02:00:36 +0100

    Idem

--- skandal/config.py
+++ skandal/config.py
@@ -24,7 +24,7 @@
 
 import os
 import ast
-from ConfigParser import SafeConfigParser
+from configparser import SafeConfigParser
 import numpy as np
 
 def load_config(ini):
@@ -67,6 +67,7 @@ def load_config(ini):
 def get_available_name(name):
     '''From raw name, get a name available to file name.'''
     name = name.encode('ascii', errors='ignore')
+    name = str(name)[1:]
     # TODO revoirpour accepter nombre si pas en premier
     name = "".join(c for c in name if c.isalpha())
     name = name[:12]
